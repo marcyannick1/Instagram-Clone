b61ba86a56416773bc4ddcfb0ac87bcdd4f5d774
Marc Yannick <yannickcoulibaly220@gmail.com>
2023-05-31 19:04:44 +0200

    login form style

--- components/Login/LoginForm.tsx
+++ components/Login/LoginForm.tsx
@@ -8,14 +8,16 @@ import {
     Link,
     Alert,
     AlertIcon,
+    Box,
 } from "@chakra-ui/react";
 import axios from "axios";
 import { useState } from "react";
 import { useRouter } from "next/router";
+import NextLink from "next/link";
 
 function LoginForm() {
     const router = useRouter();
-    
+
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
@@ -31,51 +33,83 @@ function LoginForm() {
                 password: password,
             },
         })
-        .then(function () {
-            router.push("/");
-        })
-        .catch((err) => {
-            setError(err.response.data)
-        });
+            .then(function () {
+                router.push("/");
+            })
+            .catch((err) => {
+                setError(err.response.data);
+            });
     }
 
     return (
-        <Center flexDir="column" h="100vh">
-            <Heading mb={10}>Connexion</Heading>
-            <form onSubmit={Login}>
-                <FormLabel>Email</FormLabel>
-                <Input
-                    type="email"
-                    mb={5}
-                    value={email}
-                    onChange={(e) => {
-                        setEmail(e.target.value);
-                    }}
+        <Center flexDir="column" h="100vh" textAlign="center">
+            <Center
+                border="1px"
+                borderColor="gray.300"
+                px={10}
+                py={5}
+                width={380}
+                display="flex"
+                flexDir="column"
+                mb={2}
+                rounded={2}
+            >
+                <img
+                    src="logo.png"
+                    alt=""
+                    width={180}
+                    style={{ padding: "35px 0" }}
                 />
-                <FormLabel>Mot de passe</FormLabel>
-                <Input
-                    type="password"
-                    mb={5}
-                    value={password}
-                    onChange={(e) => {
-                        setPassword(e.target.value);
-                    }}
-                />
-                <Button type="submit" w="full">
-                    {"S'inscrire"}
-                </Button>
-            </form>
-            <Text mt={5}>
-                {"Vous n'avez pas de compte? "}
-                <Link href="/register">{"S'inscrire"}</Link>
-            </Text>
-            <Link href="/mpo">Mot de passe oublié</Link>
-
-            {error &&
-            <Alert status="error" w={300}>
-                <AlertIcon />
-                {error}
-            </Alert>}
+                <form onSubmit={Login}>
+                    <Input
+                        type="email"
+                        mb={2}
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
+                        placeholder="Nom d'utilisateur ou e-mail"
+                    />
+                    <Input
+                        type="password"
+                        mb={4}
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}
+                        placeholder="Mot de passe"
+                    />
+                    <Button type="submit" w="full" mb={5} colorScheme="twitter">
+                        Se connecter
+                    </Button>
+                    {error && (
+                        <Text color="red" mb={5}>{error}</Text>
+                    )}
+                    <Link as={NextLink} href="#" color="blue.600">
+                        Mot de passe oublié
+                    </Link>
+                </form>
+            </Center>
+            <Box
+                border="1px"
+                borderColor="gray.300"
+                px={10}
+                py={5}
+                width={380}
+                rounded={2}
+            >
+                <Text>
+                    {"Vous n'avez pas de compte "}
+                    <Link
+                        as={NextLink}
+                        href="register"
+                        color="blue.400"
+                        fontWeight="medium"
+                    >
+                        Inscrivez-vous
+                    </Link>
+                </Text>
+            </Box>
         </Center>
     );
 }
